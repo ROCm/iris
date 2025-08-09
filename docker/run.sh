12a10ed22110d55d73c6a6ef8fac155300ab3ef4
Muhammad Awad <muhaawad@amd.com>
2025-08-09 04:33:55 +0000

    Address copilot comments

--- docker/run.sh
+++ docker/run.sh
@@ -9,6 +9,6 @@ WORKSPACE_DIR=${2:-"$(pwd)"}
 docker run -it --network=host --device=/dev/kfd\
             --device=/dev/dri --group-add video\
             --cap-add=SYS_PTRACE --security-opt seccomp=unconfined\
-            -v $WORKSPACE_DIR:$WORKSPACE_DIR -w $WORKSPACE_DIR\
+            -v "$WORKSPACE_DIR:$WORKSPACE_DIR" -w "$WORKSPACE_DIR"\
             --shm-size=16G --ulimit memlock=-1\
             --ulimit stack=67108864 $IMAGE_NAME
